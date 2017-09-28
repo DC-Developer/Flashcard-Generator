cdf4d334860719dc8d623a990fc3e673d04099b2
DC-Developer <dioncavanaugh@hotmail.com>
2017-09-28 10:02:05 -0400

    added bonus to clozeCard file

--- clozeCard.js
+++ clozeCard.js
@@ -1,9 +1,12 @@
 function clozeCard(text, cloze){  
-    
+    if(this instanceof clozeCard){
         this.text = text,
         this.cloze = cloze
         
-   
+    }else{
+        return new clozeCard(text,cloze);
+
+    }
 }
 clozeCard.prototype.splittingText= function(){
     var partialText = this.text.split(this.cloze);
